e5ff2a3dba42f1cc34d87606d0fbb405e8bffb41
naconn <naconn@aspen.ad.ilstu.edu>
2023-04-15 19:38:08 -0500

    Renamed folders, created base project for Java and a few makefiles

---
+++ Java/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    private String title;
+    private String author;
+    private int numCopies;
+ 
+    public Book(String title, String author, int numCopies) {
+       this.title = title;
+       this.author = author;
+       this.numCopies = numCopies;
+    }
+ 
+    public String getTitle() {
+       return title;
+    }
+ 
+    public String getAuthor() {
+       return author;
+    }
+ 
+    public int getNumCopies() {
+       return numCopies;
+    }
+ 
+    public void setNumCopies(int numCopies) {
+       this.numCopies = numCopies;
+    }
+ 
+    public void display() {
+       System.out.println("Title: " + title);
+       System.out.println("Author: " + author);
+       System.out.println("Copies available: " + numCopies);
+    }
+ }
+ 
---
+++ Java/Library.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Library {
+   private ArrayList<Book> books = new ArrayList<Book>();
+
+   public Library() {
+      books.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 3));
+      books.add(new Book("To Kill a Mockingbird", "Harper Lee", 2));
+      books.add(new Book("1984", "George Orwell", 1));
+      books.add(new Book("Fahrenheit 451", "Ray Bradbury", 1));
+   }
+
+   public void displayAvailableBooks() {
+      System.out.println("Available books:");
+      for (Book book : books) {
+         if (book.getNumCopies() > 0) {
+            book.display();
+         }
+      }
+   }
+
+   public void checkout(String title) {
+      for (Book book : books) {
+         if (book.getTitle().equalsIgnoreCase(title)) {
+            if (book.getNumCopies() > 0) {
+               book.setNumCopies(book.getNumCopies() - 1);
+               System.out.println("You have checked out \"" + title + "\". Enjoy!");
+            } else {
+               System.out.println("Sorry, there are no copies of \"" + title + "\" available.");
+            }
+            return;
+         }
+      }
+      System.out.println("Sorry, we do not have \"" + title + "\" in our library.");
+   }
+
+   public void returnBook(String title) {
+      for (Book book : books) {
+         if (book.getTitle().equalsIgnoreCase(title)) {
+            book.setNumCopies(book.getNumCopies() + 1);
+            System.out.println("You have returned \"" + title + "\". Thank you!");
+            return;
+         }
+      }
+      System.out.println("Sorry, we do not have \"" + title + "\" in our library.");
+   }
+
+   public static void main(String[] args) {
+      Library library = new Library();
+      Scanner scanner = new Scanner(System.in);
+
+      System.out.println("Hello! Welcome to our library. We hope you enjoy your visit!");
+
+      String choice = "";
+      while (!choice.equalsIgnoreCase("4")) {
+         System.out.print("What would you like to do?\n\t[1] Display available books\n\t[2] Check out a book\n\t[3] Return a book\n\t[4] Quit\nChoice:  ");
+         choice = scanner.nextLine();
+         switch (choice.toLowerCase()) {
+            case "1":
+               library.displayAvailableBooks();
+               break;
+            case "2":
+               System.out.println("Enter the title of the book you want to check out:");
+               String checkoutTitle = scanner.nextLine();
+               library.checkout(checkoutTitle);
+               break;
+            case "3":
+               System.out.println("");
+               break;
+            default:
+               break;
+         }
+      }
+
+      System.out.println("We hope you enjoyed your trip to our library!");
+      scanner.close();
+   }
+}
